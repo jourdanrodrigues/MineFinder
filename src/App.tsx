f368617a4c6de780a05bb110ea2d441b95c11835
Jourdan Rodrigues <thiagojourdan@gmail.com>
2024-09-15 11:59:07 -0300

    :sparkles: Kickstart with game in sight

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ import Grid from '@/components/Grid';
 import { useInput } from '@/hooks';
 import NumberInput from '@/components/Input';
 
+const DEFAULT_COLUMNS = 10;
+const DEFAULT_ROWS = 10;
+const DEFAULT_BOMBS = 20;
+const DEFAULT_GRID = getNewGrid({
+  rows: DEFAULT_ROWS,
+  columns: DEFAULT_COLUMNS,
+});
+
 function App() {
-  const [grid, setGrid] = useState<GridType | null>(null);
-  const [bombs, setBombs] = useInput(20);
-  const [columns, setColumns] = useInput(10);
-  const [rows, setRows] = useInput(10);
+  const [grid, setGrid] = useState<GridType>(DEFAULT_GRID);
+  const [bombs, setBombs] = useInput(DEFAULT_BOMBS);
+  const [columns, setColumns] = useInput(DEFAULT_COLUMNS);
+  const [rows, setRows] = useInput(DEFAULT_ROWS);
 
   return (
     <div className='flex flex-col items-center'>
@@ -18,21 +26,29 @@ function App() {
         <NumberInput label='Bombs' onChange={setBombs} value={bombs} />
         <NumberInput label='Columns' onChange={setColumns} value={columns} />
         <NumberInput label='Rows' onChange={setRows} value={rows} />
-        <button onClick={loadNewGame} type='button'>
+        <button
+          onClick={() => setGrid(getNewGrid({ rows, columns }))}
+          type='button'
+        >
           Start a new game
         </button>
       </div>
-      {grid && <Grid bombs={+bombs} grid={grid} />}
+      <Grid bombs={+bombs} grid={grid} />
     </div>
   );
-
-  function loadNewGame() {
-    const newGrid = range(+rows).map((x) => {
-      const cells = range(+columns).map((y) => new Cell(x, y));
-      return { id: x.toString(), cells };
-    });
-    setGrid(newGrid);
-  }
 }
 
 export default App;
+
+function getNewGrid({
+  rows,
+  columns,
+}: {
+  rows: string | number;
+  columns: string | number;
+}) {
+  return range(+rows).map((x) => {
+    const cells = range(+columns).map((y) => new Cell(x, y));
+    return { id: x.toString(), cells };
+  });
+}
